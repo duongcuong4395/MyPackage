3cff8ffcc4c21bd1ba091a71f7b546b97970492f
duongcuong4395 <dangduongcuong@gmail.com>
2025-10-28 14:56:13 +0700

    update

--- Sources/NavigationRouter/NavigationRouter.swift
+++ Sources/NavigationRouter/NavigationRouter.swift
@@ -8,32 +8,7 @@
 import SwiftUI
 import Combine
 
-// MARK: - Generic Navigation Stack
-@available(iOS 16.0, *)
-public struct GenericNavigationStack<Route: Hashable, Content: View, Destination: View>: View {
-    @ObservedObject var router: BaseRouter<Route>
-    public let rootContent: () -> Content
-    public let destination: (Route) -> Destination
-    
-    public init(router: BaseRouter<Route>, @ViewBuilder rootContent: @escaping () -> Content, @ViewBuilder destination: @escaping (Route) -> Destination
-    ) {
-        self.router = router
-        self.rootContent = rootContent
-        self.destination = destination
-    }
-    
-    public var body: some View {
-        NavigationStack(path: $router.path) {
-            rootContent()
-                .navigationDestination(for: Route.self) { route in
-                    destination(route)
-                        .environmentObject(router)
-                }
-        }
-        .environmentObject(router)
-    }
-}
-
+// MARK: - Generic Navigation Router
 @available(iOS 16.0, *)
 public struct NavigationRouter<Route: Hashable, Content: View, Destination: View>: View {
     @ObservedObject var router: BaseRouter<Route>
